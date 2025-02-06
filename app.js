320956901838570e4357e185d95047b24bf5634c
Alvaro-GF <granadosalvaro09@gmail.com>
2025-02-06 07:23:28 -0600

    funciones iniciales

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function limpiarCaja(){
+    let caja = document.getElementById("amigo")
+    caja.value = ""
+}
+
+function seleccionarAmigo(numero){
+    let amigoElegido = amigos[numero]
+    let resultado = document.getElementById
+    resultado.value = amigoElegido
+}
+
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById("amigo").value
+    limpiarCaja()
+    if (isNaN(nuevoAmigo)){
+        
+    } else {
+        amigos.push(nuevoAmigo)
+    }
+}
+
+function sortearAmigo(){
+    let numeroElegido = Math.floor(Math.random()*(amigos.length - 1))
+    if (!(numeroElegido == 0)){
+        seleccionarAmigo(numeroElegido)
+    } else {
+        
+    }
+}
